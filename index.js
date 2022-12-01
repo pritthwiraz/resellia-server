ed992704b563529aa2cb98e9ab167794b6bee4ef
pritthwiraz <pritthwiraz@gmail.com>
2022-12-01 22:06:45 +0600

    usercollection

--- index.js
+++ index.js
@@ -14,14 +14,32 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zsnxkgc.mongodb.net/?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+    try{
+        const laptopBrandsCollection = client.db('resellia').collection('laptopBrands')
+        const usersCollection = client.db('resellia').collection('users')
+
+        app.get('/laptopBrands', async(req, res) =>{
+            const query = {};
+            const options = await laptopBrandsCollection.find(query).toArray();
+            res.send(options);
+        });
+
+        app.post('/users', async(req, res) =>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+        
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.log);
 
 app.get('/', async(req, res) =>{
     res.send('resellia server is running');
