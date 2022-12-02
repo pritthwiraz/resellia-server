06bed29314f60187d4fcf69bbac9063de6c26408
pritthwiraz <pritthwiraz@gmail.com>
2022-12-02 14:53:54 +0600

    jwt verification

--- index.js
+++ index.js
@@ -5,14 +5,27 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
-
 const app = express();
-
 
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next){
+    console.log('token', req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).sent('unauthorized access')
+    }
+    const token = authHeader.split(' ')[1];
 
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zsnxkgc.mongodb.net/?retryWrites=true&w=majority`;
